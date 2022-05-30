e4b232ee5e2c749fbe767475a11dbc2f55db3edf
Miguel <a20190050@pucp.edu.pe>
2022-05-30 15:39:17 -0500

    ajustes en animal y clase perro creada

---
+++ src/pe/edu/pucp/iweb/Perro.java
@@ -0,0 +1,13 @@
+package pe.edu.pucp.iweb;
+
+public class Perro extends Animal{
+    private String modeloCorrea;
+
+    public String getModeloCorrea() {
+        return modeloCorrea;
+    }
+
+    public void setModeloCorrea(String modeloCorrea) {
+        this.modeloCorrea = modeloCorrea;
+    }
+}
